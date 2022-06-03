8fe19b910f569c409abaffefc2fc374f69da09dc
fssalazar <salazarfelipe.fs@gmail.com>
2022-06-03 13:36:02 -0300

    cypress

--- cypress/integration/01-first-flight.spec.js
+++ cypress/integration/01-first-flight.spec.js
@@ -7,9 +7,16 @@ describe('Create a New Item', () => {
     cy.visit('/jetsetter');
   });
 
-  it('should have a form', () => {});
+  it('should have a form', () => {
+    cy.get('form');
+  });
 
-  it('should have the words "Add Item"', () => {});
+  it('should have the words "Add Item"', () => {
+    cy.contains('Add Item');
+  });
 
-  it('should put stuff in an input field', () => {});
+  it('should put stuff in an input field', () => {
+    cy.get('[data-test="new-item-input"]').type('Good attitude');
+    cy.get('[data-test="add-item"]').click();
+  });
 });
